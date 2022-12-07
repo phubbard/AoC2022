be1c615f181621ab10ae05cf4e92863c1575e26e
Paul Hubbard <paul_hubbard@intuit.com>
2022-12-07 12:43:20 -0800

    first half of day five works

---
+++ 5.py
@@ -0,0 +1,91 @@
+
+"""
+This one is much harder. Lets try for some pieces first. We need two parsers
+- initial stack into memory
+- moves
+"""
+from collections import deque
+
+
+DATAFILE ='./data/5.txt'
+# Rather than writing a parser for this, just manually encode the initial conditions.
+COLS = [
+    'GDVZJSB',
+    'ZSMGVP',
+    'CLBSWTQF',
+    'HJGWMRVQ',
+    'CLSNFMD',
+    'RGCD',
+    'HGTRJDSQ',
+    'PFV',
+    'DRSTJ'
+]
+
+# Create an array of deques
+STACKS = [deque(x) for x in COLS]
+TEST_COLS = [
+    'ZN',
+    'MCD',
+    'P'
+]
+TEST_MOVES = """move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+TEST_ANSWER = 'CMZ'
+
+
+def parse_move(data_line):
+    # Could use regex but overkill
+    tokens = data_line.strip().split(' ')
+    assert tokens[0] == 'move'
+    count = int(tokens[1])
+    assert tokens[2] == 'from'
+    source = int(tokens[3]) - 1
+    assert tokens[4] == 'to'
+    dest = int(tokens[5]) - 1
+    return count,source,dest
+
+
+
+def do_move(count, source, dest, stacks):
+    for idx in range(count):
+        stacks[dest].append(stacks[source].pop())
+    return stacks
+
+
+def get_tops(stacks):
+    rc = ''
+    for col in stacks:
+        rc += col.pop()
+    return rc
+
+
+def test_step_one():
+    stacks = [deque(x) for x in TEST_COLS]
+    for move in TEST_MOVES.split('\n'):
+        if len(move) == 0:
+            continue
+
+        count,src,dest = parse_move(move)
+        do_move(count, src, dest, stacks)
+
+    rc = get_tops(stacks)
+    assert rc == TEST_ANSWER
+
+
+def step_one():
+    stacks = STACKS
+    data_lines = open(DATAFILE, 'r').readlines()
+    # Skip over initial state and blank lines
+    for line in data_lines[10:]:
+        count,src,dest = parse_move(line)
+        do_move(count, src, dest, stacks)
+    rc = get_tops(stacks)
+    print(rc)
+
+
+if __name__ == '__main__':
+    test_step_one()
+    step_one()
