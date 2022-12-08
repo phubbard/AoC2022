27a7d6c81cb202b4eedcfb39e0116ea351f78de2
Paul Hubbard <paul_hubbard@intuit.com>
2022-12-07 16:59:31 -0800

    p2 of day 5 done

--- 5.py
+++ 5.py
@@ -1,13 +1,13 @@
 
 """
-This one is much harder. Lets try for some pieces first. We need two parsers
+This one is much harder. Let's try for some pieces first. We need two parsers
 - initial stack into memory
 - moves
 """
 from collections import deque
 
 
-DATAFILE ='./data/5.txt'
+DATAFILE = './data/5.txt'
 # Rather than writing a parser for this, just manually encode the initial conditions.
 COLS = [
     'GDVZJSB',
@@ -34,6 +34,7 @@ move 2 from 2 to 1
 move 1 from 1 to 2
 """
 TEST_ANSWER = 'CMZ'
+P2_ANSWER = 'MCD'
 
 
 def parse_move(data_line):
@@ -45,14 +46,24 @@ def parse_move(data_line):
     source = int(tokens[3]) - 1
     assert tokens[4] == 'to'
     dest = int(tokens[5]) - 1
-    return count,source,dest
-
+    return count, source, dest
 
 
 def do_move(count, source, dest, stacks):
     for idx in range(count):
         stacks[dest].append(stacks[source].pop())
     return stacks
+
+
+def do_9001_move(count, src, dest, stacks):
+    elements = []
+    for idx in range(count):
+        assert len(stacks[src]) > 0
+        elements.append(stacks[src].pop())
+
+    elements.reverse()
+    stacks[dest].extend(elements)
+    pass
 
 
 def get_tops(stacks):
@@ -68,24 +79,42 @@ def test_step_one():
         if len(move) == 0:
             continue
 
-        count,src,dest = parse_move(move)
+        count, src, dest = parse_move(move)
         do_move(count, src, dest, stacks)
 
     rc = get_tops(stacks)
     assert rc == TEST_ANSWER
 
 
-def step_one():
-    stacks = STACKS
+def test_step_two():
+    stacks = [deque(x) for x in TEST_COLS]
+    for move in TEST_MOVES.split('\n'):
+        if len(move) == 0:
+            continue
+
+        count, src, dest = parse_move(move)
+        do_9001_move(count, src, dest, stacks)
+
+    rc = get_tops(stacks)
+    assert rc == P2_ANSWER
+
+
+def steps():
+    # Need two copies as the moves are in-place
+    stacks = [deque(x) for x in COLS]
+    v2_stacks = [deque(x) for x in COLS]
+
     data_lines = open(DATAFILE, 'r').readlines()
     # Skip over initial state and blank lines
     for line in data_lines[10:]:
-        count,src,dest = parse_move(line)
+        count, src, dest = parse_move(line)
         do_move(count, src, dest, stacks)
+        do_9001_move(count, src, dest, v2_stacks)
     rc = get_tops(stacks)
-    print(rc)
-
+    rc2 = get_tops(v2_stacks)
+    print(f'{rc} v1 {rc2} v2')
 
 if __name__ == '__main__':
     test_step_one()
-    step_one()
+    test_step_two()
+    steps()
