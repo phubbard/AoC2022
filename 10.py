79d92190d939008187b0b681346625ad418253d6
Paul Hubbard <paul_hubbard@intuit.com>
2022-12-11 19:17:53 -0800

    Lotsa places to make indexing errors - my favorite. Checkpointing where I pass the test.

--- 10.py
+++ 10.py
@@ -10,18 +10,45 @@ def calc_ss(signal):
         reg_val = signal[idx]
         cur_ss = idx * reg_val
         ss += cur_ss
-
     return ss
 
 
+def print_sprite(sprite_pos):
+    rc = ''
+    for x in range(40):
+        if sprite_pos in range(x - 1, x + 2):
+            rc += '#'
+        else:
+            rc += '.'
+    print(f'Sprite position: {rc}')
+
+
+def visualize_signal(signal):
+    cycle = 1
+    sprite_pos = 1
+
+    for row in range(6):
+        output_row = ''
+        for col in range(40):
+            # print_sprite(sprite_pos)
+            if col in range(sprite_pos - 1, sprite_pos + 2):
+                  output_row += '#'
+            else:
+                output_row += '.'
+            cycle += 1
+            sprite_pos = signal[cycle]
+
+        print(output_row)
+
+
 def cpu_sim():
-    signal = [1,1]
+    signal = [1, 1]
     x = 1
 
     medium_datafile = "./data/10-b.txt"
     datafile = "./data/10.txt"
 
-    for instruction in open(datafile, 'r'):
+    for instruction in open(medium_datafile, 'r'):
     # for instruction in test_program.split('\n'):
         if len(instruction) == 0:
             continue
@@ -34,10 +61,10 @@ def cpu_sim():
             x = x + int(tokens[1])
             signal.append(x)
 
-    # print(signal)
+    print(signal)
     # assert signal == [1, 1, 1, 4, 4, -1]
     print(calc_ss(signal))
-
+    visualize_signal(signal)
 
 if __name__ == '__main__':
     cpu_sim()
