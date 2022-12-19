c47a6c725ec8749f547fb32bfc09b57865816890
Paul Hubbard <paul_hubbard@intuit.com>
2022-12-19 14:06:14 -0800

    Sample data, answers for same, link to definition.

---
+++ 13.py
@@ -0,0 +1,32 @@
+
+# Problem spec https://adventofcode.com/2022/day/13
+DATAFILE = "./data/13.txt"
+
+sample_data = """
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+"""
+
+sample_answers = [True, True, False, True, False, True, False, False]
+
