38f32b779d57dcc6b2af03df0e03617781f2c9d8
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2022-12-21 12:39:44 -0800

    Cization in progress.

---
+++ do_15.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Building..."
+gcc master_15.c -o master_15.exe
+echo "Running..."
+./master_15.exe
+echo "Done, no errors."
+
+
+
---
+++ master_15.c
@@ -0,0 +1,10 @@
+
+
+#include "stdio.h"
+
+int main()
+{
+    printf("Oygodc.\n");
+    return 0;
+}
+
