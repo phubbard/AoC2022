b4c79976df5e4d77399b21878d772736db2fff98
Paul Hubbard <paul_hubbard@intuit.com>
2022-12-20 16:46:09 -0800

    starting day 14, basic algo

--- 14.py
+++ 14.py
@@ -8,3 +8,31 @@ sample_data = """
 sample_answer = 34
 
 
+def down(current):
+    return current[0] - 1, current[1]
+
+
+def down_left(current):
+    return current[0] - 1, current[1] - 1
+
+
+def down_right(current):
+    return current[0] - 1, current[1] + 1
+
+
+def drop_sand(map, min_x_val):
+    # Returns ending coordinates
+    map[500, 0] = 'o'
+    current_coordinates = (500, 0,)
+
+    while current_coordinates[0] >= min_x_val:
+        if map[down(current_coordinates)] == '.':
+            current_coordinates = down(current_coordinates)
+        elif map[down_left(current_coordinates)] == '.':
+            current_coordinates = down_left(current_coordinates)
+        elif map[down_right(current_coordinates)] == '.':
+            current_coordinates = down_right(current_coordinates)
+        else:
+            # FIXME flag ending condition. How do we discern end vs keep-here?
+            break
+    return current_coordinates
