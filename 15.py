9d26f0900a32bbc1cf9592eab0124dcf99d41ba7
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2022-12-21 11:50:42 -0800

    Rough brute force candidate set up.:

--- 15.py
+++ 15.py
@@ -1,4 +1,10 @@
+import logging
+
 DATAFILE = "./data/15.txt"
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s %(message)s')
+log = logging.getLogger()
+
 
 sample_data = """
 Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -18,3 +24,85 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3
 """
 
 sample_ans_y10 = 26
+
+
+class Sensor:
+    __slots__ = "SENSOR_MY_X", "SENSOR_MY_Y", "SENSOR_BEACON_X", "SENSOR_BEACON_Y", "SENSOR_MANHATTAN_X", "SENSOR_MANHATTAN_Y", "SENSOR_MANHATTAN_DISTANCE",
+
+    def __init__(self, sensor_line_definition):
+        log.debug(f"PARTIAL: sensor_line_definition -> {sensor_line_definition}")
+        xe_split = sensor_line_definition.split("x=")
+        ye_split = sensor_line_definition.split("y=")
+        log.debug(f"PARTIAL: xe_split -> {xe_split}")
+        log.debug(f"PARTIAL: ye_split -> {ye_split}")
+        self.SENSOR_MY_X               = int(xe_split[1].split(",")[0])
+        self.SENSOR_MY_Y               = int(ye_split[1].split(":")[0])
+        self.SENSOR_BEACON_X           = int(xe_split[2].split(",")[0])
+        self.SENSOR_BEACON_Y           = int(ye_split[2])
+        self.SENSOR_MANHATTAN_X        = abs(self.SENSOR_BEACON_X - self.SENSOR_MY_X)
+        self.SENSOR_MANHATTAN_Y        = abs(self.SENSOR_BEACON_Y - self.SENSOR_MY_Y)
+        self.SENSOR_MANHATTAN_DISTANCE = self.SENSOR_MANHATTAN_X + self.SENSOR_MANHATTAN_Y
+
+    def __str__(self):
+        return            f"Sensor at x={self.SENSOR_MY_X    }, y={self.SENSOR_MY_Y    }: " +\
+               f"closest beacon is at x={self.SENSOR_BEACON_X}, y={self.SENSOR_BEACON_Y}; " +\
+               f"distance={self.SENSOR_MANHATTAN_DISTANCE}"
+
+    def is_point_in_manhattan_range(self, test_x, test_y):
+        delta_x = abs(self.SENSOR_MY_X - test_x)
+        delta_y = abs(self.SENSOR_MY_Y - test_y)
+        manhattan = delta_x + delta_y
+        return manhattan <= self.SENSOR_MANHATTAN_DISTANCE
+
+
+def parse_input(input_string):
+    return [Sensor(line) for line in input_string.strip().split("\n")]
+
+if __name__ == '__main__':
+    if False:
+        sensor_list = parse_input(sample_data)
+    else:
+        sensor_list = parse_input(open(DATAFILE, 'r').read())
+
+    log.info("Reflecting input...")
+    for sensor in sensor_list:
+        log.debug(str(sensor))
+
+    log.info("Determine APPROXIMATE extents...")
+    min_x = sensor_list[0].SENSOR_MY_X
+    max_x = min_x
+    min_y = sensor_list[0].SENSOR_MY_Y
+    max_y = min_y
+    for sensor in sensor_list:
+        min_x = min(min_x, sensor.SENSOR_MY_X, sensor.SENSOR_BEACON_X)
+        min_y = min(min_y, sensor.SENSOR_MY_Y, sensor.SENSOR_BEACON_Y)
+        max_x = max(max_x, sensor.SENSOR_MY_X, sensor.SENSOR_BEACON_X)
+        max_y = max(max_y, sensor.SENSOR_MY_Y, sensor.SENSOR_BEACON_Y)
+    approx_extent_x = max_x - min_x
+    approx_extent_y = max_y - min_y
+    log.info(f" -> min_x:{min_x} max_y:{max_y}  -> approx_extent_x:{approx_extent_x}")
+    log.info(f" -> min_x:{min_x} max_y:{max_y}  -> approx_extent_y:{approx_extent_y}")
+
+    megabytes_approx = (approx_extent_x * approx_extent_y * 4) / 1000000
+    log.info(f"NAive full grid storage requirements: {megabytes_approx} megabytes (!!!)")
+
+    test_y_index = 10
+    too_big_to_fail = 8000000
+    log.info(f"locate along test_y_index:{test_y_index} ...")
+    covered_count = 0
+    for x in range(-too_big_to_fail, too_big_to_fail):
+        if 0 == (x % 100000): log.debug(f"at x = {x}")
+        is_covered = False
+        is_beacon  = False
+        for sensor in sensor_list:
+            is_covered = is_covered or sensor.is_point_in_manhattan_range(x, test_y_index)
+            is_beacon = is_beacon or ((x == sensor.SENSOR_BEACON_X) and (test_y_index == sensor.SENSOR_BEACON_Y))
+        if is_covered and not is_beacon:
+            covered_count += 1
+    log.info(f"    ... yields covered_count:{covered_count}")
+
+
+
+
+
+
